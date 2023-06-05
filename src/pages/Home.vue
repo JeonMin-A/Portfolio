3697a1bb8148078b64f95ad489908df8b53eb021
JeonMin-A <jeonminah13@gmail.com>
2023-06-05 11:54:11 +0900

    [feat]main home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,28 +1,33 @@
 <template>
-    <div class="uppercase max-w-7xl py-[300px] p-[2%]">
-        <div class="flex text-6xl font-bold justify-center mr-32">
-            <h2 class="mr-2">j</h2>
-            <h2 class="mr-2">e</h2>
-            <img :src="require(`@/assets/images/longCircle_icon2.svg`)" alt="" class="w-20 mt-2 mr-2">
+    <div class="uppercase max-w-7xl py-[300px] p-[3%] xl:-mt-[300px]">
+        <div class="flex text-6xl font-bold justify-center mr-32 md:text-8xl md:ml-[50px] xl:text-[200px] xl:mr-[0px]">
+            <h2 class="mr-1">j</h2>
+            <h2 class="mr-1">e</h2>
+            <img :src="require(`@/assets/images/longCircle_icon1.svg`)" alt="" class="w-[170px] md:w-[250px] mt-2 mr-1 hidden md:block xl:w-[460px]">
+            <!-- <<< sm -->
+            <img :src="require(`@/assets/images/longCircle_icon2.svg`)" alt="" class="w-20 mt-2 mr-1 md:hidden">
             <h2>n</h2>
         </div>
-        <div class="flex text-6xl font-bold justify-center ml-12">
-            <h2 class="mr-2">m</h2>
-            <h2 class="mr-2">i</h2>
-            <h2 class="mr-2">n</h2>
-            <h2 class="mr-2">-</h2>
-            <h2 class="mr-2">a</h2>
+        <div class="flex text-6xl font-bold justify-center ml-12 md:text-8xl md:mr-[110px] xl:text-[200px] xl:ml-[830px]">
+            <h2 class="mr-1">m</h2>
+            <h2 class="mr-1">i</h2>
+            <h2 class="mr-1">n</h2>
+            <h2 class="mr-1">-</h2>
+            <h2 class="mr-1">a</h2>
         </div>
-        <div class="flex text-6xl font-bold justify-center">
-            <h2 class="mr-2">p</h2>
-            <img :src="require(`@/assets/images/clover_icon.svg`)" alt="" class="w-10 mt-2 mr-2 animate-spin-slow">
-            <h2 class="mr-2">r</h2>
-            <h2 class="mr-2">t</h2>
-            <h2 class="mr-2">f</h2>
-            <img :src="require(`@/assets/images/shortCircle_icon.svg`)" alt="" class="w-8 mt-2 mr-2">
-            <h2 class="mr-2">l</h2>
-            <h2 class="mr-2">i</h2>
-            <img :src="require(`@/assets/images/shortCircle_icon.svg`)" alt="" class="w-8 mt-2">
+        <div class="flex text-6xl font-bold justify-center md:text-8xl xl:text-[200px] xl:ml-[630px]">
+            <h2 class="mr-1">p</h2>
+            <img :src="require(`@/assets/images/clover_icon.svg`)" alt="" class="w-10 mt-2 mr-1 md:w-[68px] md:mx-2 md:mt-3 animate-spin-slow xl:hidden">
+            <img :src="require(`@/assets/images/clover_icon2.svg`)" alt="" class="w-10 mt-2 mr-1 md:w-[68px] md:mx-2 md:mt-3 animate-spin-slow xl:w-[120px] xl:mx-2 hidden xl:block">
+            <h2 class="mr-1">r</h2>
+            <h2 class="mr-1">t</h2>
+            <h2 class="mr-1">f</h2>
+            <img :src="require(`@/assets/images/shortCircle_icon.svg`)" alt="" class="w-8 mt-2 mr-1 md:w-[53px] md:mt-3 xl:hidden">
+            <img :src="require(`@/assets/images/shortCircle_icon2.svg`)" alt="" class="xl:w-[110px] xl:mt-6 xl:mx-2 hidden xl:block">
+            <h2 class="mr-1">l</h2>
+            <h2 class="mr-1">i</h2>
+            <img :src="require(`@/assets/images/shortCircle_icon.svg`)" alt="" class="w-8 mt-2 md:w-[53px] md:mt-3 xl:hidden">
+            <img :src="require(`@/assets/images/shortCircle_icon2.svg`)" alt="" class="xl:w-[110px] xl:mt-6 xl:mx-2 hidden xl:block">
         </div>
     </div>
 </template>
