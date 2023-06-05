468a6152600ca266d7a6d3400b856e028db00d60
JeonMin-A <jeonminah13@gmail.com>
2023-06-05 10:10:35 +0900

    [style]main home css

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,28 @@
 <template>
-    <div class="mt-[300px] xl:mt-0 xl:py-60 flex flex-col md:flex-row justify-center my-56 gap-x-10 xl:gap-x-20">
-        <div class="imogi md:mx-0">
-            <img :src="require(`@/assets/images/imogi.svg`)" alt="이모지" class="w-[200px] xl:w-[300px] mx-auto">
+    <div class="uppercase max-w-7xl py-[300px] p-[2%]">
+        <div class="flex text-6xl font-bold justify-center mr-32">
+            <h2 class="mr-2">j</h2>
+            <h2 class="mr-2">e</h2>
+            <img :src="require(`@/assets/images/longCircle_icon2.svg`)" alt="" class="w-20 mt-2 mr-2">
+            <h2>n</h2>
         </div>
-        <div class="flex justify-center font-semibold mt-14 md:mr-8">
-            <h2 class="text-2xl dark:text-white lg:text-5xl xl:text-6xl xl:leading-[65px] tracking-wide mb-3 ">Hey!<br> My Name is Min-A,<br>and I'm a <span class="text-[#179379] dark:text-lime-400 font-bold relative">FE Developer
-                <img :src="require(`@/assets/images/hand.svg`)" alt="이모지" class="absolute -right-24 top-2 w-[80px] animate-wiggle-more animate-infinite">
-            </span>
-            </h2>
+        <div class="flex text-6xl font-bold justify-center ml-12">
+            <h2 class="mr-2">m</h2>
+            <h2 class="mr-2">i</h2>
+            <h2 class="mr-2">n</h2>
+            <h2 class="mr-2">-</h2>
+            <h2 class="mr-2">a</h2>
+        </div>
+        <div class="flex text-6xl font-bold justify-center">
+            <h2 class="mr-2">p</h2>
+            <img :src="require(`@/assets/images/clover_icon.svg`)" alt="" class="w-10 mt-2 mr-2 animate-spin-slow">
+            <h2 class="mr-2">r</h2>
+            <h2 class="mr-2">t</h2>
+            <h2 class="mr-2">f</h2>
+            <img :src="require(`@/assets/images/shortCircle_icon.svg`)" alt="" class="w-8 mt-2 mr-2">
+            <h2 class="mr-2">l</h2>
+            <h2 class="mr-2">i</h2>
+            <img :src="require(`@/assets/images/shortCircle_icon.svg`)" alt="" class="w-8 mt-2">
         </div>
     </div>
 </template>
