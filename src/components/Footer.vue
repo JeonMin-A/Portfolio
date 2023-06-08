50e0f3fe07fd6e22c3ddbc24ab9647ce41235df9
JeonMin-A <jeonminah13@gmail.com>
2023-06-08 12:13:40 +0900

    [style]M footer-area css

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="w-full bg-white dark:bg-[#1d1c21] dark:text-[#d9d9d9] py-4 text-center text-xs lg:text-lg">
+    <div class="w-full bg-white dark:bg-[#1d1c21] dark:text-[#d9d9d9] py-4 text-center text-xs lg:text-lg xl:text-sm">
         <div class="top md:my-5">
             <p class="mb-3">본 사이트의 모든 컨텐츠는 상업적 목적이 아닌 개인 포트폴리오용으로 제작되었음을 알려드립니다.</p>
             <p class="mb-3">This website's content is created for personal portfolio purposes and not for commercial use.</p>
             <p class="mb-3 md:mb-2 lg:mb-3">jeonminah01@gmail.com</p>
         </div>
-        <div class="bottom text-[10px] border-t border-t-[#999] lg:text-lg">
+        <div class="bottom text-[10px] border-t border-t-[#999] lg:text-lg xl:text-sm">
             <p class="mt-3 lg:mb-3">ⓒ 2023. Jeon Min-A all rights reserved.</p>
             <p class="text-[#999] dark:text-[#555]">LastUpdate : 2023.05.30</p>
         </div>
