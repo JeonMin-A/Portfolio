e3abeb1a02ab09e784657c9c88ec005095ab67e7
JeonMin-A <jeonminah13@gmail.com>
2023-06-08 13:11:50 +0900

    [style]M Portfolio-area css

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,15 +2,18 @@
     <Title>about</Title>
     <div class="my-14 flex flex-wrap justify-center items-center gap-10 xl:gap-16 py-[200px]">
         <div class="relative mb-5 inline-block">
-            <img :src="require(`@/assets/images/circleFrame.svg`)" alt="" class="animate-spin-slow rounded-[120px] w-[300px] overflow-visible mx-auto xl:w-[400px]">
+            <!-- lightMode -->
+            <img :src="require(`@/assets/images/circleFrame.svg`)" alt="" class="animate-spin-slow rounded-[120px] w-[300px] overflow-visible mx-auto xl:w-[400px] block dark:hidden">
+            <!-- darkMode -->
+            <img :src="require(`@/assets/images/circleFrame_white.svg`)" alt="" class="animate-spin-slow rounded-[120px] w-[300px] overflow-visible mx-auto xl:w-[400px] hidden dark:block">
             <img :src="require(`@/assets/images/profile_img.png`)" alt="" class="rounded-full w-[250px] xl:w-[350px] absolute block top-[24px] left-[26px]">
         </div>
         <div class="flex justify-center">
             <div class="text-center">
-                <h3 class="uppercase text-3xl font-bold mb-1 xl:text-5xl">jeon min-A</h3>
-                <p class="mb-3 text-sm text-gray-500 xl:text-lg xl:mb-12">2001.11.13 | Daegu</p>
-                <p class="mb-3 font-medium xl:text-3xl xl:mb-8">jeonminah01@gmail.com</p>
-                <p class="w-[300px] text-gray-500 xl:text-lg mx-auto xl:w-[400px]">예술과 컴퓨터에 관심이 많아 프론트엔드를 공부하고 있습니다.</p>
+                <h3 class="uppercase text-3xl font-bold mb-1 xl:text-5xl dark:text-[#d9d9d9]">jeon min-A</h3>
+                <p class="mb-8 text-sm text-gray-500 xl:text-lg xl:mb-12 dark:text-stone-400/30">2001.11.13 | Daegu</p>
+                <p class="mb-5 font-medium xl:text-3xl xl:mb-8 dark:text-[#d9d9d9]">jeonminah01@gmail.com</p>
+                <p class="w-[300px] text-gray-500 xl:text-lg mx-auto xl:w-[400px] dark:text-stone-400">예술과 컴퓨터에 관심이 많아 프론트엔드를 공부하고 있습니다.</p>
             </div>
         </div>
     </div>
--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -8,14 +8,14 @@
                 <!-- 2 -->
                 <div class="flex justify-center mb-20 xl:ml-9">
                     <!-- Notion / 1 -->
-                    <div class="Notion hover:opacity-70 duration-200 cursor-pointer mr-10 md:mr-32 xl:mr-10 animation_1">
+                    <div class="Notion hover:opacity-70 duration-200 cursor-pointer mr-10 md:mr-32 xl:mr-10 xl:animation_1">
                         <a href="https://jeonmin-a.notion.site/FE-9e441af27b1441908ee34ad8ab4136ed" target="_blank" class="">
                             <img :src="require(`@/assets/images/Notion_app_logo.png`)" alt="노션" class="w-[150px] md:w-40 lg:w-[300px] xl:w-[210px]">
                             <h5 class="text-center text-xl md:text-4xl mt-5 md:mt-9 dark:text-[#d9d9d9] lg:text-5xl xl:text-3xl">Notion</h5>
                         </a>
                     </div>
                     <!-- GitHub -->
-                    <div class="GitHub hover:opacity-70 duration-200 cursor-pointer animation_2">
+                    <div class="GitHub hover:opacity-70 duration-200 cursor-pointer xl:animation_2">
                         <a href="https://github.com/JeonMin-A" target="_blank" class="">
                             <img :src="require(`@/assets/images/GitHub_logo.png`)" alt="깃허브" class="w-[150px] md:w-40 lg:w-[300px] xl:w-[210px]">
                             <h5 class="text-center text-xl md:text-4xl mt-5 md:mt-9 dark:text-[#d9d9d9] lg:text-5xl xl:text-3xl">GitHub</h5>
@@ -24,14 +24,14 @@
                 </div>
                 <div class="flex justify-center mb-20">
                     <!-- mail -->
-                    <div class="mail hover:opacity-70 duration-200 cursor-pointer mr-10 md:mr-32 xl:mr-10 animation_1">
+                    <div class="mail hover:opacity-70 duration-200 cursor-pointer mr-10 md:mr-32 xl:mr-10 xl:animation_1">
                         <a href="" target="_blank" class="">
-                            <img :src="require(`@/assets/images/mail_icon.svg`)" alt="지메일" class="xl:animation_1 w-[150px] md:w-40 lg:w-[300px] xl:w-[210px]">
+                            <img :src="require(`@/assets/images/mail_icon.svg`)" alt="지메일" class=" w-[150px] md:w-40 lg:w-[300px] xl:w-[210px]">
                             <h5 class="text-center text-xl md:text-4xl mt-5 md:mt-9 dark:text-[#d9d9d9] lg:text-5xl xl:text-3xl">Gmail</h5>
                         </a>
                     </div>
                     <!-- openkakao -->
-                    <div class="mail hover:opacity-70 duration-200 cursor-pointer animation_2">
+                    <div class="mail hover:opacity-70 duration-200 cursor-pointer xl:animation_2">
                         <a href="https://open.kakao.com/o/sQ9xTLmf" target="_blank" class="">
                             <img :src="require(`@/assets/images/openkakao_icon.svg`)" alt="지메일" class="w-[150px] md:w-40 lg:w-[300px] xl:w-[210px] md:mx-auto">
                             <h5 class="text-center text-xl md:text-4xl mt-5 md:mt-9 dark:text-[#d9d9d9] lg:text-5xl xl:text-3xl">0penKakao</h5>
